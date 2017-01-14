e2124711208e62cd967a53003b98b10acd405a6e
James <caterpillarous@yahoo.com>
2017-01-14 11:27:44 +0800

    add wechat sdk

--- src/wechat/views.py
+++ src/wechat/views.py
@@ -8,13 +8,20 @@ from django.http import JsonResponse
 from django.http import HttpResponse
 import xml.dom.minidom
 from xml.etree.ElementTree import Element, SubElement, tostring
+from wechat_sdk import WechatConf
 # Create your views here.
-
+conf = WechatConf(
+    token='Caterpillarous', 
+    appid='your_appid', 
+    appsecret='your_appsecret', 
+    encrypt_mode='normal',  # 可选项：normal/compatible/safe，分别对应于 明文/兼容/安全 模式
+    encoding_aes_key='your_encoding_aes_key'  # 如果传入此值则必须保证同时传入 token, appid
+)
+from wechat_sdk import WechatBasic
+wechat = WechatBasic(conf=conf)
 
 def generate_text_reply(request):
 	user = ''
-	server = '<![CDATA[Caterpillarous]]>'
-	type = '<![CDATA[text]]>'
 	createtime = ''
 	content = ''
 	
@@ -27,7 +34,6 @@ def generate_text_reply(request):
 	createtime = node.childNodes[0].data
 	node = collection.getElementsByTagName('Content')[0]
 	content = node.childNodes[0].data
-	
 	
 	#generate
 	replytext = '''<xml>
@@ -62,13 +68,25 @@ def auth(request):
 			return response
 		else:
 			return HttpResponse(echostr)
-	else:
-		response = HttpResponse(generate_text_reply(request), content_type='application/xml; charset=utf-8')
-		return response
-			
+	elif request.method == 'POST':
+		return HttpResponse(process_msg(request))
+		#return HttpResponse(generate_text_reply(request), content_type='application/xml; charset=utf-8')
+	
 
-
-
+def process_msg(request):
+	from wechat_sdk.exceptions import ParseError
+	try:
+		wechat.parse_data(request.body)
+	except ParseError:
+		print 'Invalid Body Text'
+	id = wechat.message.id          # 对应于 XML 中的 MsgId
+	target = wechat.message.target  # 对应于 XML 中的 ToUserName
+	source = wechat.message.source  # 对应于 XML 中的 FromUserName
+	time = wechat.message.time      # 对应于 XML 中的 CreateTime
+	type = wechat.message.type      # 对应于 XML 中的 MsgType
+	content = wechat.message.content  
+	xml = wechat.response_text(content = content)
+	return xml
 
 
 
